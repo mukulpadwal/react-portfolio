feeb21029dc7365734db31df4a830167dd747fe9
mukulpadwal <mukulpadwal786@gmail.com>
2023-04-10 18:59:54 +0530

    Fixed Map Not Displaying

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -30,13 +30,13 @@ const About = () => {
                         />
                     </h1>
                     <p>
-                    Hello, my name is Mukul Padwal, and I am a fresher software engineer with experience in the MERN Stack, Java, Python, MySQL, Linux.</p>
+                    Hello, my name is Mukul Padwal, and I am a fresher software engineer with experience in the <strong>MERN Stack</strong>, <strong>Java</strong>, Python, MySQL, Linux.</p>
 
                     <p>
                     I have always been fascinated by technology and how it can solve real-world problems. This passion led me to pursue a degree in computer science, and I'm excited to be starting my career in software engineering. I enjoy the challenge of writing code and finding elegant solutions to complex problems.
                     </p>
                     
-                    <p> I am a self-motivated and detail-oriented individual who is passionate about coding and enjoys learning new things. I have a strong foundation in programming concepts and have completed several projects using these technologies. I am skilled in developing web applications, creating responsive designs, and implementing RESTful APIs. I also possess a good understanding of database management and have experience working with SQL and NoSQL databases. I am a quick learner and thrive in team environments. I am excited to utilize my skills and contribute to the success of any team that I am a part of.
+                    <p> I am skilled in developing web applications, creating responsive designs, and implementing RESTful APIs. I also possess a good understanding of database management and have experience working with SQL and NoSQL databases. I am a quick learner and thrive in team environments. I am excited to utilize my skills and contribute to the success of any team that I am a part of.
                     </p>
                 </div>
 
--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -3,73 +3,76 @@ import Loader from 'react-loaders';
 import AnimatedLetters from "../AnimatedLetters";
 import './index.scss';
 import emailjs from '@emailjs/browser';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function Contact() {
-    
+
     const [letterClass, setLetterClass] = useState('text-animate');
     const refForm = useRef();
 
     const contactStr = "Contact Me";
     const contactArr = contactStr.split("");
 
-    useEffect(function (){
-        setTimeout( function () {
+    useEffect(function () {
+        setTimeout(function () {
             setLetterClass('text-animate-hover');
         }, 3000);
     }, []);
 
-    function sendEmail(e){
+    function sendEmail(e) {
         e.preventDefault();
 
         // For anyone who needs help setting up the emailJS portion:
-    // The format should be as follows: sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY') 
-    // After creating an account, can find your service ID in the email services tab, your template ID in the email templates tab, and your public key within the Account tab under API Keys.
+        // The format should be as follows: sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY') 
+        // After creating an account, can find your service ID in the email services tab, your template ID in the email templates tab, and your public key within the Account tab under API Keys.
         emailjs
-        .sendForm(
-            process.env.REACT_APP_EMAILJS_SERVICE_ID,
-            process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
-            refForm.current,
-            process.env.REACT_APP_EMAILJS_PUBLIC_KEY
-        )
-        .then(
-            () => {
-                alert('Message Successfully sent!');
-                window.location.reload(false);
-            },
-            () => {
-                alert("Failed to send the message, please try again!");
-                window.location.reload(false);
-            }
-        );
+            .sendForm(
+                process.env.REACT_APP_EMAILJS_SERVICE_ID,
+                process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+                refForm.current,
+                process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+            )
+            .then(
+                () => {
+                    alert('Message Successfully sent!');
+                    window.location.reload(false);
+                },
+                () => {
+                    alert("Failed to send the message, please try again!");
+                    window.location.reload(false);
+                }
+            );
     }
-    
+
+    // console.log(process.env.REACT_APP_MAP_API_KEY);
+    const position = [ 32.539555, 75.970955];
+
     return (
         <>
             <div className='container contact-page'>
                 <div className='text-zone'>
                     <h1>
-                        <AnimatedLetters 
+                        <AnimatedLetters
                             letterClass={letterClass}
                             strArray={contactArr}
                             index={15}
                         />
                     </h1>
                     <p>
-                    Thank you for taking the time to learn more about me and my work. If you're interested in collaborating, have a project you'd like to discuss, or just want to say hello, please don't hesitate to get in touch! You can reach me by dropping your message below. I'm always excited to hear from like-minded individuals and potential clients, and I look forward to exploring the ways in which we can work together to create something amazing.
+                        Thank you for taking the time to learn more about me and my work. If you're interested in collaborating, have a project you'd like to discuss, or just want to say hello, please don't hesitate to get in touch! You can reach me by dropping your message below. I'm always excited to hear from like-minded individuals and potential clients, and I look forward to exploring the ways in which we can work together to create something amazing.
                     </p>
                     <div className='contact-form'>
                         <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className='half'>
-                                    <input 
-                                        type="text" 
-                                        name="name" 
-                                        placeholder='Name' 
-                                        required/>
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        placeholder='Name'
+                                        required />
                                 </li>
                                 <li className='half'>
-                                    <input 
+                                    <input
                                         type='email'
                                         name='email'
                                         placeholder='Email'
@@ -77,7 +80,7 @@ function Contact() {
                                     />
                                 </li>
                                 <li>
-                                    <input 
+                                    <input
                                         type='text'
                                         name='subject'
                                         placeholder='Subject'
@@ -85,14 +88,14 @@ function Contact() {
                                     />
                                 </li>
                                 <li>
-                                    <textarea 
+                                    <textarea
                                         placeholder='Message'
                                         name='message'
                                         required
                                     ></textarea>
                                 </li>
                                 <li>
-                                    <input 
+                                    <input
                                         type='submit'
                                         className='flat-button'
                                         value="SEND"
@@ -103,7 +106,7 @@ function Contact() {
                     </div>
                 </div>
 
-                {/* <div className='info-map'>
+                <div className='info-map'>
                     Mukul Padwal
                     <br />
                     India
@@ -113,16 +116,17 @@ function Contact() {
                     176304
                     <br />
                     <span>mukulpadwal786@gmail.com</span>
-                </div> */}
+                </div>
 
                 <div className='map-wrap'>
-                    <MapContainer center={[32.535062,75.973719]} zoom={13}>
+                    <MapContainer className="leaflet-conatiner" center={position} zoom={13} scrollWheelZoom={false}>
                         <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
-                        <Marker position={[32.535062,75.973719]}>
+                        <Marker position={position}>
                             <Popup>
-                                Mukul lives here, you can have a visit!!!
+                                Mukul lives here <br /> Come have a visit.
                             </Popup>
                         </Marker>
                     </MapContainer>
